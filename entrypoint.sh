2c92bcf6ebcaf26727877c72b81942908a95dc9b
崔庆才丨静觅 <cqc@cuiqingcai.com>
2021-06-19 14:08:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ git config --global user.email "$INPUT_USER_EMAIL"
 # install hexo env
 npm install hexo-cli -g
 npm install hexo-deployer-git --save
+npm install cross-env -g
 
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
@@ -24,10 +25,10 @@ elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
 then
     # pull original publish repo
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d
+    cross-env NODE_OPTIONS="--max-old-space-size=16384" hexo g -d
 else
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d -m "$INPUT_COMMIT_MSG"
+    cross-env NODE_OPTIONS="--max-old-space-size=16384" hexo g -d -m "$INPUT_COMMIT_MSG"
 fi
 
 echo ::set-output name=notify::"Deploy complate."
